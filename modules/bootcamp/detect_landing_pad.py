e48ab2d845cf1d6383d518c6ce14a250320abea5
exahn <exahn@uwaterloo.ca>
2023-09-07 16:46:23 -0400

    changed conf

--- modules/bootcamp/detect_landing_pad.py
+++ modules/bootcamp/detect_landing_pad.py
@@ -84,7 +84,7 @@ class DetectLandingPad:
         # * conf
         # * device
         # * verbose
-        predictions = self.__model.predict(source=image, conf=0.5, device=self.__DEVICE, verbose=False)
+        predictions = self.__model.predict(source=image, conf=0.7, device=self.__DEVICE, verbose=False)
 
         # Get the Result object
         prediction = predictions[0]
