63a8dff488ff52c56a91695157602878e01af318
Jonathan Yuan <jy90023@gmail.com>
2023-09-11 18:07:04 -0400

    complete task4

--- modules/bootcamp/decision_waypoint_landing_pads.py
+++ modules/bootcamp/decision_waypoint_landing_pads.py
@@ -6,6 +6,7 @@ Travel to designated waypoint and then land at a nearby landing pad.
 # Disable for bootcamp use
 # pylint: disable=unused-import
 
+import math
 
 from .. import commands
 from .. import drone_report
@@ -37,7 +38,7 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
         # ↓ BOOTCAMPERS MODIFY BELOW THIS COMMENT ↓
         # ============
 
-        # Add your own
+        self.timeline = "initial"
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
@@ -68,10 +69,59 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
         # ↓ BOOTCAMPERS MODIFY BELOW THIS COMMENT ↓
         # ============
 
-        # Do something based on the report and the state of this class...
+        # print(landing_pad_locations)
 
-        # Remove this when done
-        raise NotImplementedError
+        x1, y1 = report.position.location_x, report.position.location_y
+
+        # Do something based on the report and the state of this class...
+        if report.status == drone_status.DroneStatus.HALTED:
+            if self.timeline == "initial":
+                x2, y2 = self.waypoint.location_x, self.waypoint.location_y
+                relative_x = x2 - x1
+                relative_y = y2 - y1
+                command = commands.Command.create_set_relative_destination_command(relative_x, relative_y)
+
+                self.timeline = "initial-moving"
+            elif self.timeline == "initial-moving":
+                closest_x, closest_y = landing_pad_locations[0].location_x, landing_pad_locations[0].location_y
+                closest_dist = math.dist([x1, y1], [closest_x, closest_y])
+
+                for i in range(1, len(landing_pad_locations)):
+                    new_x, new_y = landing_pad_locations[i].location_x, landing_pad_locations[i].location_y
+                    new_dist = math.dist([x1, y1], [new_x, new_y])
+
+                    if new_dist < closest_dist:
+                        closest_dist = new_dist
+                        closest_x = new_x
+                        closest_y = new_y
+
+                relative_x = closest_x - x1
+                relative_y = closest_y - y1
+                command = commands.Command.create_set_relative_destination_command(relative_x, relative_y)
+
+                self.timeline = "waypoint-moving"
+                self.landing_x = closest_x
+                self.landing_y = closest_y
+            else:
+                command = commands.Command.create_land_command()
+
+                self.timeline = "landed"
+
+
+        elif report.status == drone_status.DroneStatus.MOVING:
+            if self.timeline == "initial-moving":
+                x2, y2 = self.waypoint.location_x, self.waypoint.location_y
+                dist = math.dist([x1, y1], [x2, y2])
+
+                if dist <= self.acceptance_radius:
+                    command = commands.Command.create_halt_command()
+
+            else:
+                x2, y2 = self.landing_x, self.landing_y
+                dist = math.dist([x1, y1], [x2, y2])
+
+                if dist <= self.acceptance_radius:
+                    command = commands.Command.create_halt_command()
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
