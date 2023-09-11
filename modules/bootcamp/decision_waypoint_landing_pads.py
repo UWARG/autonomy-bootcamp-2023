a743fe5c3de35836ed2fb268fc67672e15777faa
mnnaegel <maxnaegel@gmail.com>
2023-09-11 18:54:57 -0400

    Complete Task 4

--- modules/bootcamp/decision_waypoint_landing_pads.py
+++ modules/bootcamp/decision_waypoint_landing_pads.py
@@ -31,6 +31,9 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
         self.waypoint = waypoint
         print("Waypoint: " + str(waypoint))
 
+        self.waypointReached = False
+        self.targetSet = False
+        self.target = None
         self.acceptance_radius = acceptance_radius
 
         # ============
@@ -43,11 +46,18 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
         # ============
 
+    @staticmethod
+    def getDistance(a: location.Location, b: location.Location) -> float:
+        """
+        Get the distance between two locations.
+        """
+        return ((a.location_x-b.location_x)**2 + (a.location_y-b.location_y)**2)**0.5
+
     def run(self,
             report: drone_report.DroneReport,
             landing_pad_locations: "list[location.Location]") -> commands.Command:
         """
-        Make the drone fly to the waypoint and then land at the nearest landing pad.
+        Make the drone fly to the waypoint.
 
         You are allowed to create as many helper methods as you want,
         as long as you do not change the __init__() and run() signatures.
@@ -67,14 +77,42 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
         # ============
         # ↓ BOOTCAMPERS MODIFY BELOW THIS COMMENT ↓
         # ============
-
         # Do something based on the report and the state of this class...
+        if (report.status == drone_status.DroneStatus.MOVING and not self.waypointReached):
+            if (DecisionWaypointLandingPads.getDistance(report.position,self.waypoint)<self.acceptance_radius):
+                command = commands.Command.create_halt_command()
+        elif (report.status == drone_status.DroneStatus.HALTED and not self.waypointReached):
+            if (DecisionWaypointLandingPads.getDistance(report.position,self.waypoint)<self.acceptance_radius):
+                self.waypointReached = True
+                command = commands.Command.create_halt_command()
+            else:
+                command = commands.Command.create_set_relative_destination_command(self.waypoint.location_x, self.waypoint.location_y)
+        elif (self.waypointReached):
+            if (not self.targetSet):
+                closestTarget = None
 
-        # Remove this when done
-        raise NotImplementedError
+                for target in landing_pad_locations:
+                    if (closestTarget == None):
+                        closestTarget = target
+                    elif (DecisionWaypointLandingPads.getDistance(report.position, target) < DecisionWaypointLandingPads.getDistance(report.position, closestTarget)):
+                        closestTarget = target
+                self.target = closestTarget
+                self.targetSet = True
+            else:
+                if (report.status == drone_status.DroneStatus.MOVING):
+                    if (DecisionWaypointLandingPads.getDistance(report.position,self.target)<self.acceptance_radius):
+                        command = commands.Command.create_halt_command()
+                elif (report.status == drone_status.DroneStatus.HALTED):
+                    if (DecisionWaypointLandingPads.getDistance(report.position,self.target)<self.acceptance_radius):
+                        command = commands.Command.create_land_command()
+                    else:
+                        command = commands.Command.create_set_relative_destination_command(self.target.location_x-report.position.location_x, self.target.location_y-report.position.location_y)
+                else:
+                    command = commands.Command.create_null_command()
+        else:
+             command = commands.Command.create_null_command()
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
         # ============
-
         return command
