3d1e407d1dabb8cf3cb45f6932c885943c590d5d
TongguangZhang <tonggz250@gmail.com>
2023-09-19 12:19:12 -0400

    task4 land after going to waypoint

--- modules/bootcamp/decision_waypoint_landing_pads.py
+++ modules/bootcamp/decision_waypoint_landing_pads.py
@@ -37,7 +37,14 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
         # ↓ BOOTCAMPERS MODIFY BELOW THIS COMMENT ↓
         # ============
 
-        # Add your own
+        self.waypoint_found = False
+        self.landing_pad_found = False
+        self.go_to_waypoint = commands.Command.create_set_relative_destination_command(
+            waypoint.location_x,
+            waypoint.location_y
+        )
+
+        self.landed = False
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
@@ -68,10 +75,28 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
         # ↓ BOOTCAMPERS MODIFY BELOW THIS COMMENT ↓
         # ============
 
-        # Do something based on the report and the state of this class...
-
-        # Remove this when done
-        raise NotImplementedError
+        if report.status == drone_status.DroneStatus.HALTED and not self.waypoint_found:
+            print("Halted at: " + str(report.position))
+            self.waypoint_found = True
+            command = self.go_to_waypoint
+        elif report.status == drone_status.DroneStatus.HALTED and not self.landing_pad_found:
+            print("Halted at: " + str(report.position))
+            self.landing_pad_found = True
+            min_distance_squared = (60 - (-60)) ** 2 + (60 - (-60)) ** 2
+            closest_pad = 0
+            for landing_pad in landing_pad_locations:
+                distance_squared = (landing_pad.location_x - report.position.location_x) ** 2 \
+                                 + (landing_pad.location_y - report.position.location_y) ** 2
+                if distance_squared < min_distance_squared:
+                    min_distance_squared = distance_squared
+                    closest_pad = landing_pad
+            command = commands.Command.create_set_relative_destination_command(
+                closest_pad.location_x - report.position.location_x,
+                closest_pad.location_y - report.position.location_y
+            )
+            self.landing_pad_found = True
+        elif report.status == drone_status.DroneStatus.HALTED and self.landing_pad_found:
+            command = commands.Command.create_land_command()
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
