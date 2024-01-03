4b63928b6d2f11c4df6365a654a0784ea9c2675c
Jane <janezeng45@gmail.com>
2024-01-03 16:35:28 -0500

    Task 4 complete

--- modules/bootcamp/decision_waypoint_landing_pads.py
+++ modules/bootcamp/decision_waypoint_landing_pads.py
@@ -38,10 +38,40 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
         # ============
 
         # Add your own
-
+        self.command_index = 0
+        self.commands = [commands.Command.create_set_relative_destination_command(waypoint.location_x, waypoint.location_y)]
+        self.finding_landing_pad = False
+        self.has_sent_landing_command = False
+        self.counter = 0
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
         # ============
+        
+    def distance_between_two_locations_squared(self,location1: location.Location, location2: location.Location)->float:
+        '''
+        Finds the distance between two locations squared. Squared in order to avoid the heavily computational square root
+        '''
+        return (location1.location_x - location2.location_x)**2 + (location1.location_y - location2.location_y)**2
+    
+    def get_closest_landing_pad(self, current_location: location.Location, landing_pad_locations: "list[location.Location]") -> location.Location:
+        '''
+        Given the drone's current location (a.k.a waypoint), iterate over the list of landing pad locations to find the closest one
+        '''
+        closest_landing_pad_location = None
+        closest_distance = float('inf')
+        for landing_pad_location in landing_pad_locations:
+            distance = self.distance_between_two_locations_squared(current_location, landing_pad_location)
+            if distance < closest_distance:
+                closest_distance = distance
+                closest_landing_pad_location = landing_pad_location
+        return closest_landing_pad_location
+
+    def get_position_vector(self, position1: location.Location, position2: location.Location):
+        '''
+        Returns the position vector to go from location 1 to location 2
+        '''
+        return position2.location_x - position1.location_x, position2.location_y-position1.location_y
+
 
     def run(self,
             report: drone_report.DroneReport,
@@ -69,9 +99,20 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
         # ============
 
         # Do something based on the report and the state of this class...
-
-        # Remove this when done
-        raise NotImplementedError
+        if report.status == drone_status.DroneStatus.HALTED and self.command_index < len(self.commands):
+            print("Halted at: "+str(report.position))
+            command = self.commands[self.command_index]
+            self.command_index+=1
+        elif report.status == drone_status.DroneStatus.HALTED and not self.finding_landing_pad:
+            self.finding_landing_pad = True
+            if self.waypoint not in landing_pad_locations: #so if this is false, that means the drone is already at a landing pad
+                closest_landing_pad= self.get_closest_landing_pad(report.position, landing_pad_locations)
+                x, y = self.get_position_vector(report.position, closest_landing_pad)
+                command = commands.Command.create_set_relative_destination_command(x,y)
+        elif report.status == drone_status.DroneStatus.HALTED and not self.has_sent_landing_command:
+            command = commands.Command.create_land_command()
+            self.has_sent_landing_command = True
+            self.counter+=1
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
