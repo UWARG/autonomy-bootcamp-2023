7f821ac134d40c49d1577d2b2c3e334596f6422e
Kenson Hui <kenson.hui22@gmail.com>
2024-01-15 22:30:15 -0500

    Implement SOFTDEV-804072

--- modules/bootcamp/detect_landing_pad.py
+++ modules/bootcamp/detect_landing_pad.py
@@ -86,33 +86,36 @@ class DetectLandingPad:
         # * conf
         # * device
         # * verbose
-        predictions = ...
-
+        predictions = self.__model.predict(source=image)
         # Get the Result object
-        prediction = ...
+        prediction = predictions[0]
+
+        # No prediction -> return original image
+        if not prediction:
+            return ([], image)
 
         # Plot the annotated image from the Result object
         # Include the confidence value
-        image_annotated = ...
+        image_annotated = prediction.plot()
 
         # Get the xyxy boxes list from the Boxes object in the Result object
-        boxes_xyxy = ...
+        boxes_xyxy = prediction.boxes.xyxy
 
         # Detach the xyxy boxes to make a copy,
         # move the copy into CPU space,
         # and convert to a numpy array
-        boxes_cpu = ...
+        boxes_cpu = torch.Tensor.numpy(boxes_xyxy.detach().cpu())
 
         # Loop over the boxes list and create a list of bounding boxes
         bounding_boxes = []
-        # Hint: .shape gets the dimensions of the numpy array
-        # for i in range(0, ...):
+        for i in range(0, boxes_cpu.shape[0]):
             # Create BoundingBox object and append to list
-            # result, box = ...
+            result, box = bounding_box.BoundingBox.create(boxes_cpu[i])
+            if result:
+                bounding_boxes.append(box)
 
-        # Remove this when done
-        raise NotImplementedError
 
+        return (bounding_boxes, image_annotated)
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
         # ============
