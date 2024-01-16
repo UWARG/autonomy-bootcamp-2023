7bd38eddd715b0e553c66288cfdac04ed5b5c276
Ohm <ohmpatel2423@gmail.com>
2024-01-16 18:01:38 -0500

    task 4

--- modules/bootcamp/decision_waypoint_landing_pads.py
+++ modules/bootcamp/decision_waypoint_landing_pads.py
@@ -38,6 +38,10 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
         # ============
 
         # Add your own
+        self.currently_landing = False
+        self.set_destination = commands.Command.create_set_relative_destination_command(waypoint.location_x, waypoint.location_y)
+        self.count = 0
+        self.landed = False
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
@@ -69,9 +73,36 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
         # ============
 
         # Do something based on the report and the state of this class...
+        def find_distance(l1: location.Location, l2: location.Location) -> float:
+            return (l2.location_x - l1.location_x)*(l2.location_x - l1.location_x) + (l2.location_y - l1.location_y)*(l2.location_y - l1.location_y)
+        
+        def find_closest_landing_pad(curr_location: location.Location, landing_pads: "list[location.Location]") -> location.Location:
+            closest_landing_pad = None
+            closest_distance = 10000000000000
+            for index in landing_pads:
+                distance = find_distance(index, curr_location)
+                if closest_distance > distance:
+                    closest_distance = distance
+                    closest_landing_pad = index
+            return closest_landing_pad 
+        
+        if drone_status.DroneStatus.HALTED == report.status:
+            if 0 >= self.count:
+                command = self.set_destination
+                self.count += 1
+            
+            elif not self.landed:
+                self.landed = True
+                if self.waypoint not in landing_pad_locations:
+                    landing_pad = find_closest_landing_pad(report.position, landing_pad_locations)
+                    command = commands.Command.create_set_relative_destination_command (landing_pad.location_x - report.position.location_x, landing_pad.location_y - report.position.location_y)
+
+            elif not self.currently_landing:
+                self.currently_landing = True
+                command = commands.Command.create_land_command()
 
         # Remove this when done
-        raise NotImplementedError
+        #raise NotImplementedError
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
