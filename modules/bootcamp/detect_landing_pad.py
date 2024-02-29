bc7cabb1f5cbf100df9f9ba504fc591d9d5224e7
Yash Gunturi <117953649+Yash-Gunturi@users.noreply.github.com>
2024-02-29 10:48:20 -0500

    Update detect_landing_pad.py
    Edited Task 1

--- modules/bootcamp/detect_landing_pad.py
+++ modules/bootcamp/detect_landing_pad.py
@@ -86,33 +86,35 @@ class DetectLandingPad:
         # * conf
         # * device
         # * verbose
-        predictions = ...
+        predictions = self.__model.predict(source = image, conf = 0.75, device = self.__DEVICE, verbose = False)
 
         # Get the Result object
-        prediction = ...
+        prediction = predictions[0]
 
         # Plot the annotated image from the Result object
         # Include the confidence value
-        image_annotated = ...
+        image_annotated = prediction.plot(conf=True)
 
         # Get the xyxy boxes list from the Boxes object in the Result object
-        boxes_xyxy = ...
+        boxes_xyxy = prediction.xyxy
 
         # Detach the xyxy boxes to make a copy,
         # move the copy into CPU space,
         # and convert to a numpy array
-        boxes_cpu = ...
+        boxes_cpu = boxes_xyxy.cpu().numpy()
 
         # Loop over the boxes list and create a list of bounding boxes
         bounding_boxes = []
+        for i in range(0, boxes_cpu.shape()):
+            BoundingBox = i.boxes
+            bounding_boxes.append(BoundingBox)
         # Hint: .shape gets the dimensions of the numpy array
         # for i in range(0, ...):
             # Create BoundingBox object and append to list
             # result, box = ...
 
         # Remove this when done
-        raise NotImplementedError
-
+            
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
         # ============
