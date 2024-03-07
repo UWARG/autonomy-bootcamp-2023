48fe187a55e06cba8c370e663c31993aed2e3cf8
Yash <yashig0905@gmail.com>
2024-03-07 15:50:19 -0500

    testing

--- modules/bootcamp/decision_waypoint_landing_pads.py
+++ modules/bootcamp/decision_waypoint_landing_pads.py
@@ -87,7 +87,7 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
             command = commands.Command.create_set_relative_destination_command(self.closest_pad.location_x - report.position.location_x, self.closest_pad.location_y - report.position.location_y)
             self.at_local_pad = True
 
-        if report.status == drone_status.DroneStatus.HALTED and self.at_local_pad == True:
+        elif report.status == drone_status.DroneStatus.HALTED and self.at_local_pad == True:
             command = commands.Command.create_land_command()
         """   
             if report.position.location_x == self.waypoint.location_x and report.position.location_y == self.waypoint.location_y and self.at_local_pad == False:
