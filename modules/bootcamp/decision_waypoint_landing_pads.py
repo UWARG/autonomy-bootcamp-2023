db0f6fd0e7838ea2e49279c1b6757ca1356799b4
Matthew Riley <m6riley@uwaterloo.ca>
2024-06-19 09:41:33 -0700

    Final task

--- modules/bootcamp/decision_waypoint_landing_pads.py
+++ modules/bootcamp/decision_waypoint_landing_pads.py
@@ -12,7 +12,7 @@ from .. import drone_report
 from .. import drone_status
 from .. import location
 from ..private.decision import base_decision
-
+import math
 
 # Disable for bootcamp use
 # pylint: disable=unused-argument,line-too-long
@@ -32,7 +32,7 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
         print("Waypoint: " + str(waypoint))
 
         self.acceptance_radius = acceptance_radius
-
+        self.step = 0
         # ============
         # ↓ BOOTCAMPERS MODIFY BELOW THIS COMMENT ↓
         # ============
@@ -53,7 +53,6 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
         as long as you do not change the __init__() and run() signatures.
 
         This method will be called in an infinite loop, something like this:
-
         ```py
         while True:
             report, landing_pad_locations = get_input()
@@ -64,17 +63,44 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
         # Default command
         command = commands.Command.create_null_command()
 
+        
+
+
         # ============
         # ↓ BOOTCAMPERS MODIFY BELOW THIS COMMENT ↓
         # ============
 
         # Do something based on the report and the state of this class...
 
-        # Remove this when done
-        raise NotImplementedError
+        #Step 1: Travel to waypoint
+        if (self.step == 0):
+            command = commands.Command.create_set_relative_destination_command(self.waypoint.location_x, self.waypoint.location_y)
+            self.step = 1
+        elif (self.step == 1 and report.status.value == 1):
+            self.closest_pad = landing_pad_locations[0]
+            # for i in range (landing_pad_locations):
+            if (getDist(landing_pad_locations[0].location_x, landing_pad_locations[0].location_y, report.position.location_x, report.position.location_y) < getDist(self.closest_pad.location_x, self.closest_pad.location_y, report.position.location_x, report.position.location_y)):
+                self.closest_pad = landing_pad_locations[0]
+            command = commands.Command.create_set_relative_destination_command(self.closest_pad.location_x - report.position.location_x, self.closest_pad.location_y - report.position.location_y)
+            print("Moving")
+            self.step = 2
+        elif (self.step == 2 and report.status.value == 1):
+            print("Activate")
+            command = commands.Command.create_land_command()
 
+
+        # Remove this when done
+       #raise NotImplementedError
+
+        
+
+        
+        
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
         # ============
-
         return command
+
+
+def getDist(x1, y1, x2, y2):
+    return math.sqrt((x1 - x2)**2 + (y1 - y2)**2) 
