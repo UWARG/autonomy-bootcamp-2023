944567b6b3d4048a12268ef61417bfc0a5ee3165
Phiruby <vilohithrao@gmail.com>
2024-09-05 17:57:53 -0400

    Completed Task 4

--- modules/bootcamp/decision_waypoint_landing_pads.py
+++ modules/bootcamp/decision_waypoint_landing_pads.py
@@ -38,10 +38,61 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
         # ============
 
         # Add your own
+        self.waypoint_reached = False
+        self.reached_landing_pad = False
+        self.moving_to_landing_pad = False
+        self.target_landing_pad = None
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
         # ============
+    
+    def distance_to_waypoint(self, drone_location: location.Location) -> float:
+        """
+        Calculate the distance between the drone and the waypoint (unsquare-rooted).
+        """
+        return self._get_distance(drone_location, self.waypoint)
+
+    def _get_distance(self, drone_location: location.Location, waypoint: location.Location) -> float:
+        """
+        Calculate the distance between two points (unsquare-rooted).
+        """
+        return ((drone_location.location_x - waypoint.location_x)**2 + (drone_location.location_y - waypoint.location_y)**2) 
+
+    def distance_to_landing_pad(self, drone_location: location.Location, landing_pad_location: location.Location) -> float:
+        """
+        Calculate the distance between the drone and the landing pad (unsquare-rooted).
+        """
+        return self._get_distance(drone_location, landing_pad_location)
+
+    def get_relative_displacement(self, drone_location: location.Location) -> location.Location:
+        """
+        Calculate the relative displacement between the drone and the waypoint.
+        """
+        
+        return location.Location(self.waypoint.location_x - drone_location.location_x, self.waypoint.location_y - drone_location.location_y)
+
+    def get_displacement_from_landing_pad(self, drone_location: location.Location, landing_pad_location: location.Location) -> location.Location:
+        """
+        Calculate the relative displacement between the drone and the landing pad.
+        """
+        return location.Location(landing_pad_location.location_x - drone_location.location_x, landing_pad_location.location_y - drone_location.location_y)
+
+    def get_closest_landing_pad(self, landing_pad_locations: "list[location.Location]") -> location.Location:
+        """
+        Get the closest landing pad to the drone (from the waypoint).
+        """
+        # first assume the closest landing pad is the first one
+        closest_landing_pad = landing_pad_locations[0]
+        closest_distance = self.distance_to_waypoint(closest_landing_pad)
+        # now iterate and find the closest one
+        for landing_pad in landing_pad_locations:
+            distance = self.distance_to_waypoint(landing_pad)
+            if distance < closest_distance:
+                closest_landing_pad = landing_pad
+                closest_distance = distance
+
+        return closest_landing_pad
 
     def run(self,
             report: drone_report.DroneReport,
@@ -69,12 +120,61 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
         # ============
 
         # Do something based on the report and the state of this class...
+        if report.status == drone_status.DroneStatus.MOVING:
+            # if the drone is moving, we don't need to do anything
+            return command
+        
+        if self.reached_landing_pad:
+            print("Landing")
+            command = commands.Command.create_land_command()
+            return command
 
-        # Remove this when done
-        raise NotImplementedError
+        # if waypoint is reached, we find closes landing pad and go there
+        if self.waypoint_reached and not self.moving_to_landing_pad:
+            print("Reached waypoint... Determining landing pad")
+            # get closest landing pad
+            self.target_landing_pad = self.get_closest_landing_pad(landing_pad_locations)
+            print("Closest landing pad: " + str(self.target_landing_pad))
+            # relative pos of land pad from current pos
+            relative_destination_from_current_position = self.get_displacement_from_landing_pad(report.position, self.target_landing_pad)
+            self.moving_to_landing_pad = True
+            command = commands.Command.create_set_relative_destination_command(
+                relative_destination_from_current_position.location_x,
+                relative_destination_from_current_position.location_y
+            )
 
-        # ============
-        # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
-        # ============
+            print("Sending command to move to landing pad")
+            return command
+
+        # checks if the drone is within the acceptance radius
+        if not self.moving_to_landing_pad and self.distance_to_waypoint(report.position) <= self.acceptance_radius ** 2:
+            print("Waypoint reached")
+            self.waypoint_reached = True
+            command = commands.Command.create_halt_command()
+            return command
+
+        # if the drone is moving to the landing pad, check if it has reached it
+        if self.moving_to_landing_pad:
+            if self.distance_to_landing_pad(report.position, self.target_landing_pad) <= self.acceptance_radius ** 2:
+                print("Reached landing pad")
+                self.reached_landing_pad = True
+                command = commands.Command.create_halt_command()
+                return command
+
+        # if not moving and none of the above, we take off
+        if report.status == drone_status.DroneStatus.HALTED and not self.waypoint_reached:
+            print("Moving to waypoint")
+
+            relative_destination_from_current_position = self.get_relative_displacement(report.position)
+            command = commands.Command.create_set_relative_destination_command(
+                relative_destination_from_current_position.location_x,
+                relative_destination_from_current_position.location_y
+            )
+            # Remove this when done
+            # raise NotImplementedError
+
+            # ============
+            # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
+            # ============
 
         return command
