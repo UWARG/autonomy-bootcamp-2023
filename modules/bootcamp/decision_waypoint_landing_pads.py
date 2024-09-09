ec840b329cf050395c6e218d78d7db1a7ecedcff
cyuber <alyboy2006@gmail.com>
2024-09-09 19:20:31 -0400

    completed task 4

--- modules/bootcamp/decision_waypoint_landing_pads.py
+++ modules/bootcamp/decision_waypoint_landing_pads.py
@@ -38,6 +38,17 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
         # ============
 
         # Add your own
+        self.command_index = 0
+        self.commands = [
+            #commands.Command.create_set_relative_destination_command(30.0, 30.0),
+            #commands.Command.create_set_relative_destination_command(-30.0, 30.0),
+            commands.Command.create_set_relative_destination_command(self.waypoint.location_x, self.waypoint.location_y)
+        ]
+
+        self.has_sent_landing_command = False
+        self.has_found_landing_pad = False
+
+        self.counter = 0
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
@@ -69,6 +80,31 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
         # ============
 
         # Do something based on the report and the state of this class...
+        if report.status == drone_status.DroneStatus.HALTED and self.command_index < len(self.commands):
+            command = self.commands[self.command_index]
+            self.command_index += 1
+        
+        elif report.status == drone_status.DroneStatus.HALTED and not self.has_found_landing_pad:
+            dist_min = 30000
+            index = 0
+            for i in range(0, len(landing_pad_locations)):
+                dist = (landing_pad_locations[i].location_x - self.waypoint.location_x)**2+(landing_pad_locations[i].location_y - self.waypoint.location_y)**2
+                if dist < dist_min:
+                    dist_min = dist
+                    index = i
+
+            closest_landing_pad_location = landing_pad_locations[i]
+
+            command = commands.Command.create_set_relative_destination_command(closest_landing_pad_location.location_x - self.waypoint.location_x, closest_landing_pad_location.location_y - self.waypoint.location_y)
+
+            self.has_found_landing_pad = True
+
+        elif report.status == drone_status.DroneStatus.HALTED and self.has_found_landing_pad:
+
+            self.has_sent_landing_command = True
+
+        if self.has_sent_landing_command:
+            command = commands.Command.create_land_command()
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
