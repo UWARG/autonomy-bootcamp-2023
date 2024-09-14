7dab31822b478d3fa84e94b1696c97106fb7c9f3
Evan Zhao <57083990+Evang264@users.noreply.github.com>
2024-09-13 23:47:01 -0400

    Handle case when no landing pads are available

--- modules/bootcamp/decision_waypoint_landing_pads.py
+++ modules/bootcamp/decision_waypoint_landing_pads.py
@@ -37,7 +37,7 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
         # ↓ BOOTCAMPERS MODIFY BELOW THIS COMMENT ↓
         # ============
 
-        # Add your own
+        self.achieved_waypoint = False
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
@@ -68,10 +68,68 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
         # ↓ BOOTCAMPERS MODIFY BELOW THIS COMMENT ↓
         # ============
 
-        # Do something based on the report and the state of this class...
+        if not self.achieved_waypoint:
+            dx, dy = self._calc_relative_dist(report.position, self.waypoint)
+            if dx**2 + dy**2 > self.acceptance_radius**2:
+                if report.status != drone_status.DroneStatus.MOVING:
+                    command = commands.Command.create_set_relative_destination_command(dx, dy)
+            else:
+                self.achieved_waypoint = True
+        if self.achieved_waypoint:
+            # now we find the index of the landing pad closest to the drone
+            # while keeping that landing pad's distance (squared)
+
+            dx, dy = 0, 0
+            if len(landing_pad_locations) > 0:
+                # in the case that landing_pad_locations is not empty,
+                # we try to go to the nearest landing pad
+                best_index, shortest_dist = 0, self._calc_distance_squared(
+                    report, landing_pad_locations[0]
+                )
+                for i in range(1, len(landing_pad_locations)):
+                    dist = self._calc_distance_squared(report, landing_pad_locations[i])
+                    if dist < shortest_dist:
+                        best_index, shortest_dist = i, dist
+
+                dx, dy = self._calc_relative_dist(
+                    report.position, landing_pad_locations[best_index]
+                )
+            else:
+                # otherwise, we go back to the origin
+                dx, dy = -report.position.location_x, -report.position.location_y
+
+            if dx**2 + dy**2 > self.acceptance_radius**2:
+                if report.status != drone_status.DroneStatus.MOVING:
+                    command = commands.Command.create_set_relative_destination_command(dx, dy)
+            elif report.status == drone_status.DroneStatus.MOVING:
+                command = commands.Command.create_halt_command()
+            elif report.status == drone_status.DroneStatus.HALTED:
+                command = commands.Command.create_land_command()
+            # otherwise, the drone is already landed at the closest landing pad
+            # and we are done because the command is already the null command
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
         # ============
 
         return command
+
+    def _calc_distance_squared(
+        self, report: drone_report.DroneReport, location: location.Location
+    ) -> int:
+        dx, dy = self._calc_relative_dist(report.position, location)
+        return dx**2 + dy**2
+
+    def _calc_relative_dist(
+        self, loc1: location.Location, loc2: location.Location
+    ) -> "tuple[int, int]":
+        """Relative location from loc1 to loc2
+
+        Args:
+            loc1 (location.Location): Location 1
+            loc2 (location.Location): Location 2
+
+        Returns:
+            tuple[int, int]: dx, dy
+        """
+        return loc2.location_x - loc1.location_x, loc2.location_x - loc1.location_x
