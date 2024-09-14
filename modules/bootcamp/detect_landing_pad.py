a288c4b404c390c71b547caa56eb7b6eb9c3b684
bilosman <abho.osman@gmail.com>
2024-09-14 18:03:28 -0400

    Finished Task 1:   (looking forward for feedback)

--- modules/bootcamp/detect_landing_pad.py
+++ modules/bootcamp/detect_landing_pad.py
@@ -13,16 +13,8 @@ import ultralytics
 from .. import bounding_box
 
 
-# ============
-# ↓ BOOTCAMPERS MODIFY BELOW THIS COMMENT ↓
-# ============
-# Bootcampers remove the following lines:
-# Allow linters and formatters to pass for bootcamp maintainers
 # No enable
 # pylint: disable=unused-argument,unused-private-member,unused-variable
-# ============
-# ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
-# ============
 
 
 class DetectLandingPad:
@@ -98,30 +90,33 @@ class DetectLandingPad:
         # * conf
         # * device
         # * verbose
-        predictions = ...
+        predictions = self.__model.predict(source=image, conf=0.7, device="cpu")
 
         # Get the Result object
-        prediction = ...
+        prediction = predictions[0]
 
         # Plot the annotated image from the Result object
         # Include the confidence value
-        image_annotated = ...
+        image_annotated = prediction.plot(conf=True)
 
         # Get the xyxy boxes list from the Boxes object in the Result object
-        boxes_xyxy = ...
+        boxes_xyxy = prediction.boxes.xyxy
 
         # Detach the xyxy boxes to make a copy,
         # move the copy into CPU space,
         # and convert to a numpy array
-        boxes_cpu = ...
+        boxes_cpu = boxes_xyxy.detach().cpu().numpy()
 
         # Loop over the boxes list and create a list of bounding boxes
         bounding_boxes = []
         # Hint: .shape gets the dimensions of the numpy array
-        # for i in range(0, ...):
-        #     # Create BoundingBox object and append to list
-        #     result, box = ...
-
+        num_of_bounding_boxes = boxes_cpu.shape[
+            0
+        ]  # .shape => (0: num of arrays, 1: num of el in arrays)
+        for i in range(0, num_of_bounding_boxes):
+            # Create BoundingBox object and append to list
+            box = bounding_box.BoundingBox.create(boxes_cpu[i])
+            bounding_boxes.append(box)
         return [], image_annotated
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
