b8f4cabfc2d4c86b68a34d15fa30852efb775a73
Davis Liu <davisliu2006@gmail.com>
2024-09-27 23:29:22 -0400

    Task 4

--- modules/bootcamp/decision_waypoint_landing_pads.py
+++ modules/bootcamp/decision_waypoint_landing_pads.py
@@ -38,6 +38,16 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
         # ============
 
         # Add your own
+        self.command_index = 0
+        self.commands = [
+            commands.Command.create_set_relative_destination_command(
+                waypoint.location_x, waypoint.location_y
+            )
+        ]
+
+        self.has_goto_land = False
+        self.has_land = False
+        self.counter = 0
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
@@ -69,6 +79,30 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
         # ============
 
         # Do something based on the report and the state of this class...
+        pos_x = report.position.location_x
+        pos_y = report.position.location_y
+        if report.status == drone_status.DroneStatus.HALTED: # if command done
+            if self.command_index < len(self.commands): # next command
+                print(f"frame({self.counter}) cmd({self.command_index}) pos({report.position})")
+                command = self.commands[self.command_index]
+                self.command_index += 1
+            elif not self.has_goto_land: # go to landing 
+                min_dist = 1e18
+                close_x = 0; close_y = 0
+                for landing_pad in landing_pad_locations:
+                    land_x = landing_pad.location_x
+                    land_y = landing_pad.location_y
+                    dist = (pos_x-land_x)*(pos_x-land_x) + (pos_y-land_y)*(pos_y-land_y)
+                    if dist < min_dist:
+                        min_dist = dist
+                        close_x = land_x; close_y = land_y
+                command = commands.Command.create_set_relative_destination_command(close_x-pos_x, close_y-pos_y)
+                self.has_goto_land = True
+            elif not self.has_land: # start landing
+                command = commands.Command.create_land_command()
+                self.has_land = True
+
+        self.counter += 1
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
