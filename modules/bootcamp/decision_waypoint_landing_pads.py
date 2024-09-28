1a3772ed13f53da7f2a9f88bea332c19484e522d
Owen Wang <owenwang0099@gmail.com>
2024-09-28 18:00:29 -0400

    Task 4 completed

--- modules/bootcamp/decision_waypoint_landing_pads.py
+++ modules/bootcamp/decision_waypoint_landing_pads.py
@@ -38,6 +38,23 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
         # ============
 
         # Add your own
+        self.has_set_landing_command = False
+        self.waypoint_reached = False
+        self.closest_landing_pad_loc = None
+
+    def get_distance_squared(self, loc1: location.Location, loc2: location.Location) -> float:
+        """
+        Returns the distance squared between two locations.
+
+        Args:
+            loc1 (location.Location): Location 1
+            loc2 (location.Location): Location 2
+
+        Returns:
+            float: Distance squared between the two given locations
+        """
+
+        return (loc2.location_x - loc1.location_x) + (loc2.location_y - loc1.location_y)
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
@@ -69,6 +86,46 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
         # ============
 
         # Do something based on the report and the state of this class...
+        if report.status == drone_status.DroneStatus.HALTED:
+            # Drone is halted
+            if self.has_set_landing_command:
+                # Drone is not halted and has reached its destination
+                command = commands.Command.create_land_command()
+            elif self.waypoint_reached and not self.closest_landing_pad_loc:
+                shortest_dist = float("inf")
+                for loc in landing_pad_locations:
+                    dist = self.get_distance_squared(report.position, loc)
+                    if dist < shortest_dist:
+                        shortest_dist = dist
+                        self.closest_landing_pad_loc = loc
+            elif self.waypoint_reached and self.closest_landing_pad_loc:
+                command = commands.Command.create_set_relative_destination_command(
+                    self.closest_landing_pad_loc.location_x - report.position.location_x,
+                    self.closest_landing_pad_loc.location_y - report.position.location_y,
+                )
+            else:
+                command = commands.Command.create_set_relative_destination_command(
+                    self.waypoint.location_x - report.position.location_x,
+                    self.waypoint.location_y - report.position.location_y,
+                )
+        elif report.status == drone_status.DroneStatus.MOVING:
+            if (
+                self.waypoint_reached
+                and self.closest_landing_pad_loc
+                and abs(report.position.location_x - self.closest_landing_pad_loc.location_x)
+                <= self.acceptance_radius
+                and abs(report.position.location_y - self.closest_landing_pad_loc.location_y)
+                <= self.acceptance_radius
+            ):
+                command = commands.Command.create_halt_command()
+                self.has_set_landing_command = True
+            elif (
+                abs(report.position.location_x - self.waypoint.location_x) <= self.acceptance_radius
+                and abs(report.position.location_y - self.waypoint.location_y)
+                <= self.acceptance_radius
+            ):
+                command = commands.Command.create_halt_command()
+                self.waypoint_reached = True
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
