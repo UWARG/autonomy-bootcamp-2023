cdf3dcb439525a8758a1f73a212aed21c5585ba2
Hreem Pandya <109884770+HreemPandya@users.noreply.github.com>
2024-10-01 17:29:34 -0400

    Update detect_landing_pad.py

--- modules/bootcamp/detect_landing_pad.py
+++ modules/bootcamp/detect_landing_pad.py
@@ -99,7 +99,7 @@ class DetectLandingPad:
         # * device
         # * verbose
         predictions = self.__model.predict(
-            source=image, conf=0.7, device=self.__DEVICE, verbose=False  #
+            source=image, conf=0.7, device=self.__DEVICE, verbose=False
         )
 
         # Get the Result object
