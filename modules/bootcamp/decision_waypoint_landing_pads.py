f086d85e18a2537f65f7eb0162850a5f2d902bba
Bruce Wang <bruce.wang15@outlook.com>
2024-10-23 23:28:38 -0400

    changed format v2

--- modules/bootcamp/decision_waypoint_landing_pads.py
+++ modules/bootcamp/decision_waypoint_landing_pads.py
@@ -46,15 +46,17 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
         # ============
 
-    def get_nearest_landing(self, position: location.Location, landing_pad_locations: "list[location.Location]"):
+    def get_nearest_landing(
+        self, position: location.Location, landing_pad_locations: "list[location.Location]"
+    ):
         nearest_pad = None
         for pad in landing_pad_locations:
             dx = pad.location_x - position.location_x
             dy = pad.location_y - position.location_y
-            distance = (dx)**2 + (dy)**2
+            distance = (dx) ** 2 + (dy) ** 2
             if nearest_pad == None:
                 nearest_pad = [pad.location_x, pad.location_y]
-            elif distance < (nearest_pad[0]**2 + nearest_pad[1]**2):
+            elif distance < (nearest_pad[0] ** 2 + nearest_pad[1] ** 2):
                 nearest_pad = [pad.location_x, pad.location_y]
         return nearest_pad
 
@@ -94,36 +96,41 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
 
         if not self.reachedWaypoint:
             if status == drone_status.DroneStatus.HALTED:
-                if distance_from_waypoint_x <= self.acceptance_radius and distance_from_waypoint_y <= self.acceptance_radius:
+                if (
+                    distance_from_waypoint_x <= self.acceptance_radius
+                    and distance_from_waypoint_y <= self.acceptance_radius
+                ):
                     self.reachedWaypoint = True
                     nearest_pad = self.get_nearest_landing(position, landing_pad_locations)
                     self.padx = nearest_pad[0]
                     self.pady = nearest_pad[1]
-                    print('finding pads...', nearest_pad)
+                    print("finding pads...", nearest_pad)
                 elif abs(waypoint.location_x) <= 60 and abs(waypoint.location_y) <= 60:
-                    command = commands.Command.create_set_relative_destination_command(distance_from_waypoint_x, distance_from_waypoint_y)
+                    command = commands.Command.create_set_relative_destination_command(
+                        distance_from_waypoint_x, distance_from_waypoint_y
+                    )
             elif status == drone_status.DroneStatus.MOVING:
                 if report.destination == report.position:
                     command = commands.Command.create_halt_command()
-                    print('Drone is moving')
+                    print("Drone is moving")
 
         # if at waypoint
         else:
-            print('accessed here', status)
+            print("accessed here", status)
             if status == drone_status.DroneStatus.HALTED:
-                print('access here too', self.padx, self.pady)
+                print("access here too", self.padx, self.pady)
                 dx = self.padx - position.location_x
                 dy = self.pady - position.location_y
-                print('accessed here 3')
+                print("accessed here 3")
                 if dx <= self.acceptance_radius and dy <= self.acceptance_radius:
                     command = commands.Command.create_land_command()
-                    print('Landing at pad...')
+                    print("Landing at pad...")
                 elif self.padx <= 60 and self.pady <= 60:
                     command = commands.Command.create_set_relative_destination_command(dx, dy)
             elif status == drone_status.DroneStatus.MOVING:
                 if report.destination == report.position:
                     command = commands.Command.create_halt_command()
-                    print('Drone done moving')
+                    print("Drone done moving")
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
         # ============
