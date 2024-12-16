93d38f5b81fb7465fa45910fae64bc3daba5796e
Baighasan <baig.hasan@outlook.com>
2024-12-16 16:52:21 -0500

    Completed task 3

--- modules/bootcamp/decision_waypoint_landing_pads.py
+++ modules/bootcamp/decision_waypoint_landing_pads.py
@@ -19,6 +19,19 @@ from ..private.decision import base_decision
 # pylint: disable=duplicate-code,unused-argument
 
 
+def calculate_distance(x1: float, y1: float, x2: float, y2 :float) -> float:
+    """
+    Calculates the distance between two given points
+    :param x1:
+    :param y1:
+    :param x2:
+    :param y2:
+    :return:
+    """
+    distance = ((x2 - x1) ** 2 + (y2 - y1) ** 2)  ** 0.5
+    return distance
+
+
 class DecisionWaypointLandingPads(base_decision.BaseDecision):
     """
     Travel to the designed waypoint and then land at the nearest landing pad.
@@ -37,7 +50,7 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
         # ↓ BOOTCAMPERS MODIFY BELOW THIS COMMENT ↓
         # ============
 
-        # Add your own
+        self.__destination_type = ""
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
@@ -68,7 +81,41 @@ class DecisionWaypointLandingPads(base_decision.BaseDecision):
         # ↓ BOOTCAMPERS MODIFY BELOW THIS COMMENT ↓
         # ============
 
-        # Do something based on the report and the state of this class...
+        distance_from_waypoint = calculate_distance(report.position.location_x, report.position.location_y, self.waypoint.location_x, self.waypoint.location_y)
+        if self.__destination_type == "landing_pad":
+            distance_from_pad = calculate_distance(report.position.location_x, report.position.location_y, report.destination.location_x, report.destination.location_y)
+            print("Verifying distance from landing pad")
+            if report.status.value == 1:
+                command = commands.Command.create_land_command()
+                print("Reached landing pad, landing")
+            elif distance_from_pad < self.acceptance_radius:
+                command = commands.Command.create_halt_command()
+                print("Reached landing pad, halting")
+
+
+        elif distance_from_waypoint < self.acceptance_radius:
+            if report.status.value == 1:
+                min_distance = float('inf')
+                closest_pad = None
+                for l in landing_pad_locations:
+                    x1, y1 = report.position.location_x, report.position.location_y
+                    x2, y2 = l.location_x, l.location_y
+                    distance = calculate_distance(x1, y1, x2, y2)
+                    if distance < min_distance:
+                        min_distance = distance
+                        closest_pad = location.Location(x2, y2)
+
+                command = commands.Command.create_set_relative_destination_command(closest_pad.location_x - report.position.location_x, closest_pad.location_y - report.position.location_y)
+                print("Destination set to landing pad:", closest_pad.location_x - report.position.location_x, closest_pad.location_y - report.position.location_y)
+                self.__destination_type = "landing_pad"
+
+            else:
+                command = commands.Command.create_halt_command()
+                print("Waypoint reached, halting")
+        elif report.status.value == 1:
+            command = commands.Command.create_set_relative_destination_command(self.waypoint.location_x, self.waypoint.location_y)
+            print("Destination set to waypoint")
+            self.__destination_type = "waypoint"
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
