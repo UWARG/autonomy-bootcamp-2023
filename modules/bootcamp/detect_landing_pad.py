e85bbe8737ab486409f07a71850d477b186eae17
Gurvir Randhawa <gurvirrandhawa28@gmail.com>
2024-12-19 16:19:42 -0500

    Results

--- modules/bootcamp/detect_landing_pad.py
+++ modules/bootcamp/detect_landing_pad.py
@@ -101,11 +101,11 @@ class DetectLandingPad:
         predictions = self.__model.predict(source = image, conf = 0, device = self.__DEVICE, verbose = False)
 
         # Get the Result object
-        prediction = ...
+        prediction = predictions[0]
 
         # Plot the annotated image from the Result object
         # Include the confidence value
-        image_annotated = ...
+        image_annotated = prediction.plot(conf = False)
 
         # Get the xyxy boxes list from the Boxes object in the Result object
         boxes_xyxy = ...
