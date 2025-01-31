0d4fa1fbed233915b3e72317ebf6ed4efc8c9ab2
Oliad Rundassa <runol@utschools.ca>
2025-01-31 00:19:27 -0500

    task 2 & 3 passed

--- modules/bootcamp/decision_simple_waypoint.py
+++ modules/bootcamp/decision_simple_waypoint.py
@@ -39,6 +39,17 @@ class DecisionSimpleWaypoint(base_decision.BaseDecision):
 
         # Add your own
 
+        self.command = commands.Command.create_set_relative_destination_command(waypoint.location_x, waypoint.location_y)
+
+        self.command_index = 0
+        self.commands = [
+            commands.Command.create_set_relative_destination_command(self.waypoint.location_x, self.waypoint.location_y)
+        ]
+
+        self.has_sent_landing_command = False
+
+        self.counter = 0
+
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
         # ============
@@ -69,6 +80,16 @@ class DecisionSimpleWaypoint(base_decision.BaseDecision):
         # ============
 
         # Do something based on the report and the state of this class...
+
+        if report.status == drone_status.DroneStatus.HALTED and self.command_index < len(self.commands):
+            command = self.commands[self.command_index]
+            self.command_index += 1
+        elif report.status == drone_status.DroneStatus.HALTED and ((report.position.location_x - self.waypoint.location_x) > self.acceptance_radius or (report.position.location_y - self.waypoint.location_y) > self.acceptance_radius):
+            self.counter += 1
+        else:
+            command = commands.Command.create_land_command()
+            self.has_sent_landing_command = True
+
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
