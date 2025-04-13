0d791ae180e7eced7e7349aaddc0157541cd51d0
happyface3650 <syuen2010113@email.com>
2025-04-13 15:33:22 -0400

    fixed formatting issues.

--- modules/bootcamp/detect_landing_pad.py
+++ modules/bootcamp/detect_landing_pad.py
@@ -99,7 +99,9 @@ class DetectLandingPad:
         # * device
         # * verbose
 
-        predictions = self.__model.predict(source=image, conf=0.5, device=self.__DEVICE, verbose=False)
+        predictions = self.__model.predict(
+            source=image, conf=0.5, device=self.__DEVICE, verbose=False
+        )
 
         # Get the Result object
         prediction = predictions[0]
