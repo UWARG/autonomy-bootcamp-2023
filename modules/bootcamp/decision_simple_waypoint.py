d9246306b1ee96f7998f54476412208ab7730438
Sirtaj Sing <s398sing@uwaterloo.ca>
2025-04-18 17:11:29 -0400

    Complete task 1 and 2

--- modules/bootcamp/decision_simple_waypoint.py
+++ modules/bootcamp/decision_simple_waypoint.py
@@ -68,7 +68,65 @@ class DecisionSimpleWaypoint(base_decision.BaseDecision):
         # ↓ BOOTCAMPERS MODIFY BELOW THIS COMMENT ↓
         # ============
 
+        if report.status == drone_status.DroneStatus.HALTED:
+            #Things to check for a halted drone:
+            # Does it have a valid destination
+            # If no, set destanation and movemenet
+            #
+            # If yes, 
+            #   - Check if in acceptable radius of destination
+            #   - If yes, land
+            #   - Otherwise, restart movement to destination   
+            #
+            
+            pass
+            
+        elif report.status == drone_status.DroneStatus.MOVING:
+            # Check if drone is proximity of destination
+            # IF no,
+            # Confirm that the drone is moving in the correct direction: Takes constant time to do
+
+            
+            pass
+        else:
+            # Landed case
+
+
+        # CHeck if the drone has a valid destination
+        # If not, set to waypoint in init command
+        # Set global destination to that landing pad cords
+
+        # Create a new function to calculate the direction to the desired location
+        # If the drone is already moving to that position, then allow it to continue
+        
+        # If drone is halted check that it is in the acceptable radius of the waypoint
+        # Use euclidian distance to check that it is the case
+
+        # Land once in the correct position
+    
+
+
+
+        # So we have a report to play around wiht and locations
+
         # Do something based on the report and the state of this class...
+
+        # valid statuses:
+        # - Halted
+        # - Moving
+        # - Landed
+
+        # - Report contains position and global destination
+        # - For halted and landed drones, position and global dest are equal
+
+        # Four actions
+
+        # - Null: default, do nothing
+        # - set relative dest: move drone to distance RELATIVE to current pos
+        #       - Note: requires drone to be halted to use
+
+        # - Halt: Makes drone stop immediately at current position
+        # - Land: Lands drone at current position and ends simulation ( requires drone to be halted)
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
