b5b8fbfd8e7c3ac220c09ee330fdc6458635e441
Kerelos Gayed <kerelosgayed@gmail.com>
2025-07-23 03:44:41 -0400

    Removed test print statement

--- modules/bootcamp/detect_landing_pad.py
+++ modules/bootcamp/detect_landing_pad.py
@@ -99,7 +99,6 @@ class DetectLandingPad:
         predictions = self.__model.predict(
             source=image, conf=0.7, device=self.__DEVICE, verbose=False
         )
-        print(f"Predictions: {predictions}")
 
         # Get the Result object
         prediction = predictions[0]
